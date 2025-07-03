658ead1e5c3ee1f54601dfeb5ca4003518620842
MatheusIFSP <eudolly19@gmail.com>
2025-07-03 22:34:37 +0000

    início do service do produto

--- src/service/ProductService.ts
+++ src/service/ProductService.ts
@@ -2,5 +2,6 @@ import { Product } from "../model/Product"
 import { ProductRepository } from "../repository/ProductRepository"
 
 export class ProductService{
-    private productRepository = ProductRepository.getInstance()
+    private productRepository = ProductRepository
+
 }
