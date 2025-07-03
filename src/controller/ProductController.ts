97faf9d27c30e108a0965642685b51639d4d7ca8
MatheusIFSP <eudolly19@gmail.com>
2025-07-03 22:58:19 +0000

    realização do controller do produto

---
+++ src/controller/ProductController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { ProductService } from "../service/ProductService";
+import { Product } from "../model/Product";
+
+export class ProductController{
+    private productService = new ProductService()
+
+    cadastrarProduto(req: Request, res: Response){
+        try{
+            this.productService.createProduct(req.body)
+            res.status(201).json(
+                {
+                    message: 'Produto criado com sucesso!'
+                }
+            )
+        }catch(err){
+            const message = err instanceof Error ? err.message : 'Erro ao criar o produto'
+            res.status(400).json(
+                {
+                    message: message,
+                }
+            )
+        }
+    }
+}
