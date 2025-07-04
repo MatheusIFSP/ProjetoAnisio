c5ea7e3f3aa382b5e1758ebcbdb369161b615b17
MatheusIFSP <eudolly19@gmail.com>
2025-07-04 22:54:50 +0000

    correção do swagger e correção do controller do produto

--- src/controller/ProductController.ts
+++ src/controller/ProductController.ts
@@ -5,14 +5,10 @@ import { Product } from "../model/Product";
 export class ProductController{
     private productService = new ProductService()
 
-    cadastrarProduto(req: Request, res: Response){
+    async cadastrarProduto(req: Request, res: Response){
         try{
-            this.productService.createProduct(req.body)
-            res.status(201).json(
-                {
-                    message: 'Produto criado com sucesso!'
-                }
-            )
+            const newProduct = await this.productService.createProduct(req.body)
+            res.status(201).json(newProduct)
         }catch(err){
             const message = err instanceof Error ? err.message : 'Erro ao criar o produto'
             res.status(400).json(
